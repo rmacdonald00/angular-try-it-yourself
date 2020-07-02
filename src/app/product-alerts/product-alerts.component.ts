f5ebdff7dea54f77ca5b0949ddea8fa2fa802bd1
rmacdonald00 <rjmacd20@gmail.com>
2020-07-02 16:09:51 -0500

    routing and product details

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,7 +7,7 @@ import { Output, EventEmitter } from '@angular/core';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductalertsComponent implements OnInit {
+export class ProductAlertsComponent implements OnInit {
   @Input() product; 
   @Output() notify = new EventEmitter();
   constructor() { }
